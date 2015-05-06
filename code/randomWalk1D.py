9522f70b8ea9b27f9ee4d376b53b2e8ddb5e7d58
Andy Wickert <andy@northernwidget.com>
2015-05-06 13:47:54 +0200

    Started on first example

---
+++ code/randomWalk1D.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python
+
+import numpy as np # Numerical library
+from matplotlib import pyplot as plt # Plotting library
+
+# Written for Computational Methods in Earth Sciences class
+# To be emailed out after class
