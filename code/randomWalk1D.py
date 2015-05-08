6f592826100255d4137a4b42571ebe54453df2a5
Andy Wickert <andy@northernwidget.com>
2015-05-08 16:59:56 +0200

    appropriate bin for even/odd hop

--- code/randomWalk1D.py
+++ code/randomWalk1D.py
@@ -7,10 +7,10 @@ import time
 # Written for Computational Methods in Earth Sciences class
 
 # Input
-ntracers = 5000
+ntracers = 1000
 stepsize = 1
-update_period = 10 # update every this many time steps
-plot_period = 10  # Plot every this many time steps
+update_period = 5 # update every this many time steps
+plot_period = 50  # Plot every this many time steps
 final_time_step = 5000
 
 # Setup
@@ -21,6 +21,7 @@ tracer_id = np.arange(ntracers) + 1 # 1--500; can't do this with just range()
                                     # because + with lists is concatenation
                                     # instead, would be range(1, 501)
 x = np.arange(np.floor(-2*np.sqrt(final_time_step)), np.ceil(2*np.sqrt(final_time_step)))
+binwidth = 10 # For histogram
 
 fig = plt.figure(figsize=(6,9)) # Create a figure OBJECT named "fig"
 ax1 = fig.add_subplot(211)
@@ -35,7 +36,7 @@ ax2.set_xlabel('Distance from starting point') # This could also be "plt.xlabel"
 plt.ylabel('Frequency of occurrence here') # illustrating other way to do this
 # I have set xlim and ylim based on what I know about diffusion
 ax2.set_xlim(-2*np.sqrt(final_time_step), 2*np.sqrt(final_time_step))
-plt.ylim(0, ntracers/5.)
+plt.ylim(0, ntracers*binwidth/30.)
 plt.tight_layout() # Automatically c
 fig.canvas.draw()
 #plt.show(block=False) # Allow script to keep running while plot is shown
@@ -54,28 +55,19 @@ for t in range(1, final_time_step + 1):
     ax1.set_xlim(-2*np.sqrt(final_time_step), 2*np.sqrt(final_time_step))
     ax1.set_ylabel('Tracer ID')
     ax2.set_xlim(-2*np.sqrt(final_time_step), 2*np.sqrt(final_time_step))
-    ax2.set_ylim(0, ntracers/5.)
+    ax2.set_ylim(0, ntracers*binwidth/30.)
     # Plot again
     ax1.plot(tracers, tracer_id, 'ko', alpha=.25)
     # plt.hist works like ax.hist, just on current axes
     # so if we had multiple axes up, we would have to specify which one to use.
     # (hint: we do in this case)
     # Easier to name them at the start
-    ax2.hist(tracers, bins=np.arange(-100, 101, 5), color='b') # Histogram
+    ax2.hist(tracers, bins=np.arange(-250, 251, binwidth), color='b') # Histogram
     # Floor division with integer update_period is good for this
-    ax2.plot(x, 2*ntracers/(t/update_period)**.5 * \
+    ax2.plot(x, binwidth*ntracers/(2 * np.pi * t/update_period)**.5 * \
              np.exp(-x**2/(2.*(t/update_period))), color='0.', linewidth=2)
     plt.draw()
     #time.sleep(0.1) # Time to look at plot
     
 plt.show()
-
-
-
-
-
-
-
-
-
 
