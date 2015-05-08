Fix unit conversion error

code/snowball.py
@@ -38,7 +38,7 @@ class Snowball(object):
     Let's find its horizontal impact momentum when hitting a stopped object
     """
     # And we don't need to return anything, because this c
-    self.p = self.density * (4/3.) * np.pi * (self.diameter/2./1000.)**3 \
+    self.p = self.density * (4/3.) * np.pi * (self.diameter/2./100.)**3 \
                             * self.release_velocity # [N s]
 
   def travel_distance(self):
@@ -55,5 +55,17 @@ class Snowball(object):
     Print output
     """
     print self.name, 'travels', round(self.x,1), 'meters and strikes its target'
-    print 'with a momentum of', round(self.p,5), 'Newton-seconds.'
+    print 'with a momentum of', round(self.p,1), 'Newton-seconds.'
+
+
+
+
+
+
+
+
+
+
+
+
 

code/snowball_functions_module.py
@@ -9,7 +9,7 @@ def horizontal_momentum(rho, D):
   """
   Let's find its horizontal impact momentum when hitting a stopped object
   """
-  return rho * (4/3.) * np.pi * (D/2./1000.)**3 # [N]
+  return rho * (4/3.) * np.pi * (D/2./100.)**3 # [N]
 
 def travel_distance(rv, rh):
   """
@@ -27,5 +27,5 @@ def print_output(n, x, p):
   Print output
   """
   print n, 'travels', round(x,1), 'meters and strikes its target'
-  print 'with a momentum of', round(p,5), 'Newton-seconds.'
+  print 'with a momentum of', round(p,1), 'Newton-seconds.'
 

code/snowball_imperative.py
@@ -11,8 +11,8 @@ release_velocity = 15 # [m/s]
 release_height = 1.5 # [m]
 
 # Let's find its horizontal impact momentum when hitting a stopped object
-p_horizontal = density * (4/3.) * np.pi * (diameter/2./1000.)**3 \
-               * release_velocity                                   # [N s]
+p_horizontal = density * (4/3.) * np.pi * (diameter/2./100.)**3 \
+               * release_velocity                           # [N s]
 
 # Let's see how far it will go before hitting the ground
 # Ignoring wind resistance
@@ -24,5 +24,5 @@ x_flight = t * release_velocity
 # Print output
 print ""
 print name, 'travels', round(x_flight,1), 'meters and strikes its target'
-print 'with a momentum of', round(p_horizontal,5), 'Newton-seconds.'
+print 'with a momentum of', round(p_horizontal,1), 'Newton-seconds.'
 print ""

code/snowball_imperative_functions.py
@@ -14,7 +14,7 @@ def horizontal_momentum(rho, D, rv):
   """
   Let's find its horizontal impact momentum when hitting a stopped object
   """
-  return rho * (4/3.) * np.pi * (D/2./1000.)**3 * rv # [N s]
+  return rho * (4/3.) * np.pi * (D/2./100.)**3 * rv # [N s]
 
 def travel_distance(rv, rh):
   """
@@ -32,11 +32,10 @@ def print_output(n, x, p):
   Print output
   """
   print n, 'travels', round(x,1), 'meters and strikes its target'
-  print 'with a momentum of', round(p,5), 'Newton-seconds.'
+  print 'with a momentum of', round(p,1), 'Newton-seconds.'
 
 
 # First run
-print "Run 1:"
 name = 'standard snowball'
 diameter = 10 # [cm]
 density = 450 # [kg / m^3]
@@ -69,3 +68,19 @@ print "Run 4:"
 print_output('wicked tiny iceball', travel_distance(35, 1.5), \
              horizontal_momentum(917, 7, 35) )
 print ""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+